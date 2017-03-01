ccb3e3931fb90005f0b6349dfd6e2b13fe5079df
mmihael <mihael.mamula@gmail.com>
2017-03-01 12:38:50 -0500

    Jqurey ajax

---
+++ js/ajax_jquery.js
@@ -0,0 +1,9 @@
+$.ajax({ 
+  method: "POST", 
+  url: "http://www.example.com", 
+  data: "[]", 
+  headers: { 
+      "Content-Type": "application/json"
+    }, 
+  success: function (res) { } 
+})
